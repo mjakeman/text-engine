7bfb23d1230d1deaab0965d620fbb8ffe276849d
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-02-03 03:09:53 +1300

    widget: Implement basic layout and rendering

--- demo/widget.c
+++ demo/widget.c
@@ -35,6 +35,8 @@
 struct _RichTextWidget
 {
     GtkWidget parent_instance;
+
+    TextFrame *frame;
 };
 
 G_DEFINE_FINAL_TYPE (RichTextWidget, rich_text_widget, GTK_TYPE_WIDGET)
@@ -91,6 +93,69 @@ rich_text_widget_set_property (GObject      *object,
 }
 
 static void
+rich_text_widget_snapshot (GtkWidget   *widget,
+                           GtkSnapshot *snapshot)
+{
+    RichTextWidget *self = RICH_TEXT_WIDGET (widget);
+
+    TextFrame *frame = self->frame;
+    PangoContext *context = gtk_widget_get_pango_context (widget);
+
+    GdkRGBA fg_color;
+    gtk_style_context_get_color (gtk_widget_get_style_context (widget), &fg_color);
+
+    // Two things need to happen here.
+    //
+    // Firstly, we need to layout the frame probably into some form of
+    // layout tree. This can be cached between redraws so only the
+    // changed block nodes need to be recalculated.
+    //  -> Introduce an auxiliary TextLayout object
+    //  -> Uses Pango (recursively?) for determining extents
+    //
+    // Secondly, the actual drawing is implementation-specific. The data
+    // model provides stylistic information (data attributes) and semantic
+    // structure, but the actual presentation depends on other factors such
+    // as 'default stylesheet' and reflow.
+    //  -> This is implemented by this widget
+
+    for (TextNode *node = text_node_get_first_child (TEXT_NODE (frame));
+         node != NULL;
+         node = text_node_get_next (node))
+    {
+        g_assert (TEXT_IS_BLOCK (node));
+
+        // Let's treat paragraphs opaquely for now. In the future, we need
+        // to manually consider each text run in order for inline equations
+        // and images.
+        if (TEXT_IS_PARAGRAPH (node))
+        {
+            GString *str = g_string_new ("");
+            for (TextNode *run = text_node_get_first_child (node);
+                 run != NULL;
+                 run = text_node_get_next (run))
+            {
+                const gchar *run_text;
+                g_object_get (run, "text", &run_text, NULL);
+                g_string_append (str, run_text);
+            }
+
+            int height;
+            gchar *text = g_string_free (str, FALSE);
+
+            PangoLayout *layout = pango_layout_new (context);
+            pango_layout_set_text (layout, text, -1);
+            pango_layout_get_pixel_size (layout, NULL, &height);
+
+            gtk_snapshot_append_layout (snapshot, layout, &fg_color);
+            gtk_snapshot_translate (snapshot, &GRAPHENE_POINT_INIT (0, height));
+
+            g_object_unref (layout);
+            g_free (text);
+        }
+    }
+}
+
+static void
 rich_text_widget_class_init (RichTextWidgetClass *klass)
 {
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
@@ -98,22 +163,30 @@ rich_text_widget_class_init (RichTextWidgetClass *klass)
     object_class->finalize = rich_text_widget_finalize;
     object_class->get_property = rich_text_widget_get_property;
     object_class->set_property = rich_text_widget_set_property;
+
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+    widget_class->snapshot = rich_text_widget_snapshot;
 }
 
 static void
 rich_text_widget_init (RichTextWidget *self)
 {
-    TextFrame *frame = text_frame_new ();
+    self->frame = text_frame_new ();
 
     TextParagraph *block1 = text_paragraph_new ();
     text_paragraph_append_run (block1, text_run_new ("This is some text."));
-    text_frame_append_block (frame, TEXT_BLOCK (block1));
+    text_paragraph_append_run (block1, text_run_new (" Comprised of multiple runs"));
+    text_paragraph_append_run (block1, text_run_new (" which could each have their own..."));
+    text_paragraph_append_run (block1, text_run_new (" FORMATTING!"));
+    text_paragraph_append_run (block1, text_run_new (" (yay)"));
+    text_frame_append_block (self->frame, TEXT_BLOCK (block1));
 
     TextParagraph *block2 = text_paragraph_new ();
     text_paragraph_append_run (block2, text_run_new ("Some more text!"));
-    text_frame_append_block (frame, TEXT_BLOCK (block2));
+    text_frame_append_block (self->frame, TEXT_BLOCK (block2));
 
     TextParagraph *block3 = text_paragraph_new ();
     text_paragraph_append_run (block3, text_run_new ("One final paragraph :D"));
-    text_frame_append_block (frame, TEXT_BLOCK (block3));
+    text_frame_append_block (self->frame, TEXT_BLOCK (block3));
 }
