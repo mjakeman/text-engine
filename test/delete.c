af6306db44ff1d3218011f5c931fb84d042ca3a6
Matthew Jakeman <mjakeman26@outlook.co.nz>
2022-04-22 05:08:57 +1200

    test: Greatly expand deletion unit tests

--- test/delete.c
+++ test/delete.c
@@ -32,11 +32,15 @@ typedef struct {
     TextRun *run1;
     TextRun *run2;
     TextRun *run3;
+    TextRun *run4;
+    TextParagraph *para1;
+    TextParagraph *para2;
 } DeleteFixture;
 
 #define RUN1 "abcdefghij"
 #define RUN2 "1234567890"
 #define RUN3 "!@#$%^&*()"
+#define RUN4 "zxcvbnm,./"
 
 static void
 delete_fixture_set_up (DeleteFixture *fixture,
@@ -44,20 +48,22 @@ delete_fixture_set_up (DeleteFixture *fixture,
 {
     TextFrame *frame;
     TextParagraph *para1, *para2;
-    TextRun *run1, *run2, *run3;
+    TextRun *run1, *run2, *run3, *run4;
 
     frame = text_frame_new ();
 
     para1 = text_paragraph_new ();
     run1 = text_run_new (RUN1);
     run2 = text_run_new (RUN2);
+    run3 = text_run_new (RUN3);
     text_paragraph_append_run (para1, run1);
     text_paragraph_append_run (para1, run2);
+    text_paragraph_append_run (para1, run3);
     text_frame_append_block (frame, TEXT_BLOCK (para1));
 
     para2 = text_paragraph_new ();
-    run3 = text_run_new (RUN3);
-    text_paragraph_append_run (para2, run3);
+    run4 = text_run_new (RUN4);
+    text_paragraph_append_run (para2, run4);
     text_frame_append_block (frame, TEXT_BLOCK (para2));
 
     fixture->doc = text_document_new ();
@@ -70,6 +76,9 @@ delete_fixture_set_up (DeleteFixture *fixture,
     fixture->run1 = run1;
     fixture->run2 = run2;
     fixture->run3 = run3;
+    fixture->run4 = run4;
+    fixture->para1 = para1;
+    fixture->para2 = para2;
 }
 
 static void
@@ -91,6 +100,121 @@ test_within_run (DeleteFixture *fixture,
 
     g_object_get (fixture->run1, "text", &text, NULL);
     g_assert_cmpstr (text, ==, "abhij");
+}
+
+static void
+test_whole_run_others_in_paragraph (DeleteFixture *fixture,
+                                    gconstpointer  user_data)
+{
+    gchar *text;
+    int length;
+
+    // deletes the contents of run 2
+    text_editor_move_right (fixture->editor, TEXT_EDITOR_CURSOR, 10);
+    text_editor_delete (fixture->editor, TEXT_EDITOR_CURSOR, 10);
+
+    // run 2 should no longer exist
+    g_assert_true (text_editor_get_run (fixture->editor, TEXT_EDITOR_CURSOR) == fixture->run1);
+    g_assert_cmpint (fixture->doc->cursor->index, ==, 10);
+
+    // check length
+    length = text_paragraph_get_length (fixture->para1);
+    g_assert_cmpint (length, ==, 20);
+
+    // check cursor position
+    g_assert_cmpint (fixture->doc->cursor->index, ==, 10);
+
+    // check text
+    g_object_get (fixture->run1, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "abcdefghij");
+
+    g_object_get (fixture->run3, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "!@#$%^&*()");
+}
+
+static void
+test_whole_run_last_in_paragraph (DeleteFixture *fixture,
+                                  gconstpointer  user_data)
+{
+    gchar *text;
+    int length;
+
+    // deletes the contents of run 4
+    text_editor_move_right (fixture->editor, TEXT_EDITOR_CURSOR, 31);
+    text_editor_delete (fixture->editor, TEXT_EDITOR_CURSOR, 10);
+
+    // run 4 should still exist with a length of zero
+    // but one index position
+    length = text_paragraph_get_length (fixture->para2);
+    g_assert_cmpint (length, ==, 0);
+
+    // check cursor position
+    g_assert_true (text_editor_get_run (fixture->editor, TEXT_EDITOR_CURSOR) == fixture->run4);
+    g_assert_cmpint (fixture->doc->cursor->index, ==, 0);
+
+    // check text
+    g_object_get (fixture->run4, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "");
+}
+
+static void
+test_across_runs_single (DeleteFixture *fixture,
+                         gconstpointer  user_data)
+{
+    gchar *text;
+    int length;
+
+    text_editor_move_right (fixture->editor, TEXT_EDITOR_CURSOR, 8);
+    text_editor_delete (fixture->editor, TEXT_EDITOR_CURSOR, 5);
+
+    length = text_paragraph_get_length (fixture->para1);
+    g_assert_cmpint (length, ==, 25);
+
+    g_object_get (fixture->run1, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "abcdefgh");
+
+    g_object_get (fixture->run2, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "4567890");
+}
+
+static void
+test_across_runs_multiple (DeleteFixture *fixture,
+                           gconstpointer  user_data)
+{
+    gchar *text;
+    int length;
+
+    text_editor_move_right (fixture->editor, TEXT_EDITOR_CURSOR, 8);
+    text_editor_delete (fixture->editor, TEXT_EDITOR_CURSOR, 15);
+
+    length = text_paragraph_get_length (fixture->para1);
+    g_assert_cmpint (length, ==, 15);
+
+    g_object_get (fixture->run1, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "abcdefgh");
+
+    g_object_get (fixture->run3, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "$%^&*()");
+}
+
+static void
+test_run_boundary (DeleteFixture *fixture,
+                   gconstpointer  user_data)
+{
+    gchar *text;
+    int length;
+
+    text_editor_move_right (fixture->editor, TEXT_EDITOR_CURSOR, 10);
+    text_editor_delete (fixture->editor, TEXT_EDITOR_CURSOR, 1);
+
+    length = text_paragraph_get_length (fixture->para1);
+    g_assert_cmpint (length, ==, 29);
+
+    g_object_get (fixture->run1, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "abcdefghij");
+
+    g_object_get (fixture->run2, "text", &text, NULL);
+    g_assert_cmpstr (text, ==, "234567890");
 }
 
 static void
@@ -118,10 +242,31 @@ main (int argc, char *argv[])
                 delete_fixture_set_up, test_within_run,
                 delete_fixture_tear_down);
 
+    // Test whole run
+    g_test_add ("/text-engine/editor/delete/test-whole-run-others-in-paragraph", DeleteFixture, NULL,
+                delete_fixture_set_up, test_whole_run_others_in_paragraph,
+                delete_fixture_tear_down);
+    g_test_add ("/text-engine/editor/delete/test-whole-run-last-in-paragraph", DeleteFixture, NULL,
+                delete_fixture_set_up, test_whole_run_last_in_paragraph,
+                delete_fixture_tear_down);
+
     // Test across runs
+    g_test_add ("/text-engine/editor/delete/test-across-runs-single", DeleteFixture, NULL,
+                delete_fixture_set_up, test_across_runs_single,
+                delete_fixture_tear_down);
+    g_test_add ("/text-engine/editor/delete/test-across-runs-multiple", DeleteFixture, NULL,
+                delete_fixture_set_up, test_across_runs_multiple,
+                delete_fixture_tear_down);
+
     // Test run boundary
+    g_test_add ("/text-engine/editor/delete/test-run-boundary", DeleteFixture, NULL,
+                delete_fixture_set_up, test_run_boundary,
+                delete_fixture_tear_down);
+
+
     // Test across paragraphs
     // Test paragraph boundary
+    // Test delete vs backspace
     // Test inverse
     // Test nothing
     g_test_add ("/text-engine/editor/delete/test-nothing", DeleteFixture, NULL,
