304bff6304d4356bbbde4f5abed9cf2b24e4a2ca
Matthew Jakeman <mjakeman26@outlook.co.nz>
2023-01-31 00:05:59 +1100

    gtk: Create simple gtk-rs app

--- ports/gtk/src/main.rs
+++ ports/gtk/src/main.rs
@@ -1,5 +1,48 @@
 use text_engine;
 
+use gtk::prelude::*;
+use gtk::{ApplicationWindow, Button};
+use adw::Application;
+
+const APP_ID: &str = "com.mattjakeman.TextEngine";
+
 fn main() {
+    // Initialise text engine
     text_engine::init();
+
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
 }
